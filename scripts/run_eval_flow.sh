ad78d6603dc5915fe7f129cd6639c77ceba34d4f
shaoyuc3 <105130338+shaoyuc3@users.noreply.github.com>
2022-10-05 17:22:52 -0500

    Update run_eval_flow.sh

--- scripts/run_eval_flow.sh
+++ scripts/run_eval_flow.sh
@@ -1,12 +1,11 @@
-export OUTPUT_DIR='/shared/rsaas/shaoyuc3/dior-flowstyle/checkpoints_fs/PBAFN_stage1_fs/generate_test'
+export OUTPUT_DIR='checkpoints_fs/PBAFN_stage1_fs/generate'
 export NAME=PBAFN_stage1_fs
 export LOAD_EP=latest
 export NET_G=dior
 export NET_E=adgan
 export NGF=32
-export DATAROOT=/shared/rsaas/shaoyuc3/dior-flowstyle/data
-#export WARP_CHECKPOINT_DIR='/shared/rsaas/shaoyuc3/dior_style/checkpoints_fs/PBAFN_stage1_fs'
-export PRETRAINED_FLOWNET_PATH='/shared/rsaas/shaoyuc3/dior-flowstyle/checkpoints_fs/PBAFN_stage1_fs/PBAFN_warp_epoch_101.pth'
+export DATAROOT=...
+export PRETRAINED_FLOWNET_PATH='checkpoints_fs/PBAFN_stage1_fs/PBAFN_warp_epoch_101.pth'
 
 
 # generate images
